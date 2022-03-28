3d03cb104089437d82ec64cbc068047c38531722
sasjakoning <sasjakoning@hotmail.com>
2022-03-28 20:36:42 +0200

    added: gulp-nodemon for easy reloads

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 const gulp = require("gulp")
 const concat = require("gulp-concat")
+const nodemon = require("gulp-nodemon")
 
 gulp.task("default", (done) => {
+    var stream = nodemon({
+        script: "app.js",
+        ext: "html js css",
+        ignore: "style.min.css",
+        tasks: ["concat"]
+    })
+    done()
+
+    // stream.on("restart", () => {
+    //     console.log("restarted!")
+    // })
+    // .on("crash", () => {
+    //     console.error("Application has crashed!\n")
+    //     stream.emit("restart", 10)
+    // })
+})
+
+gulp.task("concat", (done) => {
     console.log("concating and moving all css files in css folder")
     gulp.src("public/css/**.css")
         .pipe(concat("style.min.css"))
