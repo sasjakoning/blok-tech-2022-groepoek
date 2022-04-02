de5a97d7c6dd9c9f83157e1429691021baefd35f
sasjakoning <sasjakoning@hotmail.com>
2022-04-02 12:40:48 +0200

    changed: uncommented scripts

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,31 @@
+// fix for transitions firing on load
+window.addEventListener("load", () => {
+  document.querySelector("body").classList.remove("preload");
+  console.log("removed preload");
+});
 
-// // fix for transitions firing on load
-// window.addEventListener("load", () => {
-//   document.querySelector("body").classList.remove("preload");
-//   console.log("removed preload");
-// });
+/****************/
+/* like dislike */
+/****************/
 
-// /****************/
-// /* like dislike */
-// /****************/
+const actionOverlay = document.querySelector(".actionOverlay");
+const actionOverlayImg = actionOverlay.querySelector("img");
 
-// const actionOverlay = document.querySelector(".actionOverlay");
-// const actionOverlayImg = actionOverlay.querySelector("img");
-
-// const card = document.querySelector(".cardLabel:first-of-type");
+const card = document.querySelector(".cardLabel:first-of-type");
 
 const likeForm = document.querySelector(".form-like");
 
 const otherCards = document.querySelectorAll(".cardLabel:not(:first-of-type)")
 
 
-// likeForm.addEventListener("submit", (e) => {
-//   console.log("submitted");
+likeForm.addEventListener("submit", (e) => {
+  console.log("submitted");
 
-//   e.preventDefault();
+  e.preventDefault();
 
-//   card.classList.add("cardLike");
-//   actionOverlay.classList.add("actionLiked");
-//   actionOverlayImg.src = "/images/overlayLike.svg";
+  card.classList.add("cardLike");
+  actionOverlay.classList.add("actionLiked");
+  actionOverlayImg.src = "/images/overlayLike.svg";
 
   otherCards.forEach((card) => {
     card.classList.add("cardsMove")
@@ -37,13 +36,12 @@ const otherCards = document.querySelectorAll(".cardLabel:not(:first-of-type)")
   });
 });
 
+const dislikeForm = document.querySelector(".form-dislike");
 
-// const dislikeForm = document.querySelector(".form-dislike");
+dislikeForm.addEventListener("submit", (e) => {
+  console.log("submitted");
 
-// dislikeForm.addEventListener("submit", (e) => {
-//   console.log("submitted");
-
-//   e.preventDefault();
+  e.preventDefault();
 
   card.classList.add("cardDislike");
   actionOverlay.classList.add("actionLiked");
@@ -58,20 +56,19 @@ const otherCards = document.querySelectorAll(".cardLabel:not(:first-of-type)")
   });
 });
 
+// match popup
+const continueBtn = document.querySelector(".continue");
+const matchBackground = document.querySelector(".matchBackground");
+const matchPopup = document.querySelector(".matchPopup");
 
-// // match popup
-// const continueBtn = document.querySelector(".continue");
-// const matchBackground = document.querySelector(".matchBackground");
-// const matchPopup = document.querySelector(".matchPopup");
-
-// //check if element exists
-// if (matchBackground != null) {
-//   continueBtn.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     matchBackground.remove();
-//     matchPopup.remove();
-//   });
-// }
+//check if element exists
+if (matchBackground != null) {
+  continueBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    matchBackground.remove();
+    matchPopup.remove();
+  });
+}
 
 window.onload=function dragOverAvatarUploadField() {
     const dropZone = document.querySelector(".avatarUploadField");
@@ -79,47 +76,23 @@ window.onload=function dragOverAvatarUploadField() {
     const HuidigeAvatar = document.querySelector(".eigenAvatarfotoGr");
     const uploadFotoOverlay = document.querySelector(".uploadFotoOverlay");
 
-    const profilePageGlobal = document.querySelector(".profilePage")
-    const editButton = document.querySelector(".profilePage > img:first-of-type")
-    // const formRoepnaam = document.querySelector("#formRoepnaam");
-    // const formOmschrijving = document.querySelector("formOmschrijving");
-    // const formLeeftijd = document.querySelector("formLeeftijd");
-    // const formPlaats = document.querySelector("#formPlaats");
-    // const formLengte = document.querySelector("formLengte");
-
     dropZone.addEventListener("dragover", e => {
         e.preventDefault();
-        dropZone.classList.add("hover");
         stockFotoInDropZone.classList.add("hidden");
         HuidigeAvatar.classList.add("transparent50");
         uploadFotoOverlay.classList.remove("hidden");
     })
 
     dropZone.addEventListener("dragleave", e => {
-        dropZone.classList.remove("hover");
         stockFotoInDropZone.classList.remove("hidden");
         HuidigeAvatar.classList.remove("transparent50");
         uploadFotoOverlay.classList.add("hidden");
     })
 
     dropZone.addEventListener("dragend", e => {
-        dropZone.classList.remove("hover");
         stockFotoInDropZone.classList.remove("hidden");
         HuidigeAvatar.classList.remove("transparent50");
         uploadFotoOverlay.classList.add("hidden");
-    })
-    dropZone.addEventListener("mouseover", e => {
-        stockFotoInDropZone.classList.add("hidden");
-        HuidigeAvatar.classList.add("transparent50");
-        uploadFotoOverlay.classList.remove("hidden");
-    })
-    dropZone.addEventListener("mouseout", e => {
-      stockFotoInDropZone.classList.remove("hidden");
-      HuidigeAvatar.classList.remove("transparent50");
-      uploadFotoOverlay.classList.add("hidden");
-  })
-    editButton.addEventListener("click", e => {
-      profilePageGlobal.classList.add("editingProfile");
     })
 }
 
@@ -131,4 +104,3 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
-
