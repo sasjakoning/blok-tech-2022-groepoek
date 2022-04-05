d58342a7bbe82433977df893f5d70e8b3dd4eab4
03Sandraa <sandra@heijnekamp.nl>
2022-04-05 19:16:47 +0200

    added: error handling

--- routes/profile.js
+++ routes/profile.js
@@ -7,29 +7,37 @@ const adminUserModel = require("../models/adminUser");
 const mongoose = require("mongoose");
 const toId = mongoose.Types.ObjectId;
 const multer = require("multer")
-const { authenticate } = require('../config/auth');
+const {
+  authenticate
+} = require('../config/auth');
 
 
 // ---
 
 const router = express.Router();
-const upload = multer({dest: "public/uploads/"})
+const upload = multer({
+  dest: "public/uploads/"
+})
 
+const error = new Error("De volgende errors hebben plaatsgevonden:")
 
 router.get("/", authenticate, profile);
 
 async function profile(req, res) {
-  const userid = req.session.userid
-  const currentUser = await userModel.findOne({
-    email: userid,
-  }).lean();
+  try {
+    const userid = req.session.userid
+    const currentUser = await userModel.findOne({
+      email: userid,
+    }).lean();
 
 
     res.render("profile", {
       layout: "index",
       data: currentUser,
     });
-
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 
@@ -38,67 +46,79 @@ async function profile(req, res) {
 router.post("/updateprofile", upload.none(), updateProfile);
 
 async function update(userid, updatedata) {
+  try {
+    const result = await userModel.updateOne({
+      email: userid
+    }, {
+      $set: updatedata
+    }, {
+      upsert: true
+    });
+  } catch (error) {
+    console.error(error)
+  }
 
-  const result = await userModel.updateOne(
-    { email: userid },
-    { $set: updatedata },
-    { upsert: true }
-  );
 }
 
 async function updateProfile(req, res) {
-  const userid = req.session.userid
-  const currentUser = await userModel.findOne({
-    email: userid,
-  }).lean();
+  try {
+    const userid = req.session.userid
+    const currentUser = await userModel.findOne({
+      email: userid,
+    }).lean();
 
-  
-  update(userid, {
-    firstname: req.body.voornaam,
-    aboutme: req.body.omschrijving,
-    gender: req.body.geslacht,
-    age: req.body.leeftijd,
-    location: req.body.plaats,
-    height: req.body.lengte,
-    // interests: req.body.interesses,
-    platform: {
-      discord: req.body.discord,
-      xbox: req.body.xbox,
-      playstation: req.body.playstation,
-      whatsapp: req.body.whatsapp,
-      messenger: req.body.messenger,
-      skype: req.body.skype,
-    },
-  });
+
+    update(userid, {
+      firstname: req.body.voornaam,
+      aboutme: req.body.omschrijving,
+      gender: req.body.geslacht,
+      age: req.body.leeftijd,
+      location: req.body.plaats,
+      height: req.body.lengte,
+      // interests: req.body.interesses,
+      platform: {
+        discord: req.body.discord,
+        xbox: req.body.xbox,
+        playstation: req.body.playstation,
+        whatsapp: req.body.whatsapp,
+        messenger: req.body.messenger,
+        skype: req.body.skype,
+      },
+    });
 
     res.render("profile", {
       layout: "index",
       data: currentUser,
     });
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 router.post("/avatarupdate", upload.single("avatar"), avatarUpdate);
 
 async function avatarUpdate(req, res) {
-  const userid = req.session.userid
-  const currentUser = await userModel.findOne({
-    email: userid,
-  }).lean();
-  
-  const avatar = (await "uploads/") + req.file.filename;
+  try {
+    const userid = req.session.userid
+    const currentUser = await userModel.findOne({
+      email: userid,
+    }).lean();
 
-  update(userid,{
-    images: {
-      avatar: avatar,
-    }
-  });
+    const avatar = (await "uploads/") + req.file.filename;
 
-  getUsers().then(([result, admin, adminLeaned]) => {
+    update(userid, {
+      images: {
+        avatar: avatar,
+      }
+    });
+
     res.render("profile", {
       layout: "index",
       data: currentUser,
     });
-  });
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 
