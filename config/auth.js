716c73bbfb39a3e9939b674e8a9d0468d7b4e60c
sasjakoning <sasjakoning@hotmail.com>
2022-04-06 20:03:52 +0200

    changed: now check for user in database

--- config/auth.js
+++ config/auth.js
@@ -1,21 +1,30 @@
 let sessions = require("express-session");
+const userModel = require("../models/user");
 
 let currentsession;
 
 // module maken
 module.exports = {
   // de functie is authenticate
-  authenticate: function(req, res, next){
+  authenticate: async function(req, res, next){
     // haal de cookie op van client side
     currentsession=req.session;
     console.log(`session cookie is`)
     console.log(currentsession)
 
+
+    const userid = req.session.userid
+    const currentUser = await userModel.findOne({
+      email: userid,
+    }).lean();
+
     // als de cookie een userid heeft, ga verder
-    if(currentsession.userid){
+    if(currentUser != null){
+      console.log("auth valid")
       return next()
     }
 
+    console.log("auth invalid")
     // als de cookie geen userid heeft, redirect naar login
     res.redirect("/login")
   }
